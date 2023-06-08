5a6f325533e93424d52c9f05e22dee68f78e0861
fakcil <akcil16itu@gmail.com>
2023-06-08 12:42:14 +0300

    Adding Header

--- reactRanked/src/components/navi/Header.js
+++ reactRanked/src/components/navi/Header.js
@@ -1,10 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavbarToggler, Collapse,
+         Nav, NavItem, NavLink, } from "reactstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export default class Header extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false,
+        };
+      }
+      toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen,
+        });
+      }
   render() {
     return (
       <div>
-        <h1>Header</h1>
+        <Navbar color="light" light expand="md">
+            <NavbarBrand>
+                
+                <FontAwesomeIcon icon={faHome} /> Home
+                
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse
+            isOpen={this.state.isOpen}
+            navbar
+            style={{ justifyContent: "flex-end", marginRight: "100px" }}
+            >
+                
+                <Nav className="mr-auto" navbar>
+                <NavItem>
+                        <NavLink>
+                            
+                            <FontAwesomeIcon icon={faUser} /> User
+                            
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                
+                <Nav className="ml-auto" navbar>
+                    
+                    <NavItem>
+                        <NavLink>
+                            
+                            <FontAwesomeIcon icon={faShoppingCart} /> Sepet
+                            
+                        </NavLink>
+                    </NavItem>
+                    
+                </Nav>
+            </Collapse>
+        </Navbar>
       </div>
     )
   }
